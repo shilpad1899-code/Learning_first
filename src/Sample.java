a2659dff6f170c9163cfc003c86ab19b6cc5d38b
shilpad1899-code <shilpad1899@gmail.com>
2025-11-10 11:36:18 -0500

    Learning2

--- src/Sample.java
+++ src/Sample.java
@@ -4,7 +4,7 @@ public class Sample {
     public static void main(String[] args) {
         System.out.println("Welcome Shilpa");
         for(int i=1;i<=10;i++){
-             System.out.println("I am  " +i);
+             System.out.println("I am shilpa, how are you  " +i);
         }
     }
 }
