9251029fa8c462363861ce4598c3c6de8dec5a8c
shilpad1899-code <shilpad1899@gmail.com>
2025-11-10 11:24:08 -0500

    Learning1

---
+++ src/Sample.java
@@ -0,0 +1,10 @@
+import java.util.stream.Stream;
+
+public class Sample {
+    public static void main(String[] args) {
+        System.out.println("Welcome Shilpa");
+        for(int i=1;i<=10;i++){
+             System.out.println("I am  " +i);
+        }
+    }
+}
---
+++ src/Sample_two.java
@@ -0,0 +1,14 @@
+public class Sample_two {
+    public static void main(String[] args) {
+        System.out.println("Sample_two");
+        int a=2;
+        int b=3;
+        int c;
+        c=a+b;
+
+            System.out.println("Sample_two result is "+ c);
+
+    }
+
+
+}
